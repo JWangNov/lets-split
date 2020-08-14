17d00fa7fb80fb9799009dbb8a84625603b931b9
JWangNov <wangjialun8421@gmail.com>
2020-08-13 20:48:01 -0400

    Create BillService

---
+++ src/main/java/com/jw/letssplit/service/BillService.java
@@ -0,0 +1,22 @@
+package com.jw.letssplit.service;
+
+import com.jw.letssplit.po.Bill;
+
+import java.util.List;
+
+public interface BillService {
+
+    List<Bill> listAllBill();
+
+    List<Bill> listBillOfAUser(Integer userId);
+
+    Bill getBill(Integer id);
+
+    int deleteBill(Integer id);
+
+    void deleteBill(List<Integer> ids);
+
+    int createBill(Bill bill);
+
+    int updateBill(Integer id, Bill bill);
+}
