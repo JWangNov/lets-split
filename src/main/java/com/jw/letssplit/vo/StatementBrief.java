3a4abbd001e172d0e3e46a344b3fc22b20a6ba72
JWangNov <wangjialun8421@gmail.com>
2020-08-14 21:55:55 -0400

    Create StatementController

--- src/main/java/com/jw/letssplit/vo/StatementBrief.java
+++ src/main/java/com/jw/letssplit/vo/StatementBrief.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import java.util.Map;
 
 @Data
-public class BillStatementBrief {
+public class StatementBrief {
     private User owner;
     private Map<User, Double> payeeBalanceMap;
 }
